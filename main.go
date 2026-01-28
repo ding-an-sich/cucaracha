dcb65282fac98b5d5c79eb674ba0b0a1b9502de6
vmrc <vmrc@ding-an-sich.com>
2026-01-27 22:27:37 -0300

    setup profiling directives

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"strings"
 )
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 	var (
@@ -18,6 +24,18 @@ func main() {
 	flag.StringVar(&brokers, "brokers", "", "Comma-separated Kafka broker addresses (required)")
 	flag.StringVar(&topic, "topic", "", "Topic to read from (required)")
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	if brokers == "" || topic == "" {
 		fmt.Fprintln(os.Stderr, "Error: --brokers, --topic, are required")
@@ -41,5 +59,20 @@ func main() {
 	if err := encoder.Encode(result); err != nil {
 		fmt.Fprintf(os.Stderr, "Error encoding JSON: %v\n", err)
 		os.Exit(1)
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		// Lookup("allocs") creates a profile similar to go test -memprofile.
+		// Alternatively, use Lookup("heap") for a profile
+		// that has inuse_space as the default index.
+		if err := pprof.Lookup("allocs").WriteTo(f, 0); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
 	}
 }
